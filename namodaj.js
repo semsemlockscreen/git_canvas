e5ebdd98cb1ffe09832338b03c8415370cea2c04
semsemlockscreen <70825069+semsemlockscreen@users.noreply.github.com>
2020-09-09 23:35:45 +0100

    Update namodaj.js

--- namodaj.js
+++ namodaj.js
@@ -17766,11 +17766,7 @@ p.nominalBounds = new cjs.Rectangle(-425.3,-76.7,850.6,144.3);
 			});
 		
 		
-		
-		
-		
-		
-		
+		alert('v = 20');
 		
 			vid = document.getElementById('vi');
 			source = document.createElement('source');
